31be92ed031f212a5f99b1c50af44e58bd3d0b36
LLeo93 <liba.leoncini@gmail.com>
2025-03-21 16:19:36 +0100

    commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,6 @@
 const getGames = function () {
   const gameURL = 'https://striveschool-api.herokuapp.com/api/product/';
-  //   con questo URL ora facciamo un'operazione di GET per recuperare gli eventi
-  // attualmente salvati
+
   fetch(gameURL, {
     headers: {
       Authorization:
@@ -10,25 +9,16 @@ const getGames = function () {
     },
   })
     .then((response) => {
-      // response è un oggetto JS che comprende un po' di proprietà interessanti
-      // tra cui una proprietà chiamata "ok" che in un semplice booleano riassume
-      // l'esito della chiamata
       if (response.ok) {
-        // possiamo sperare di recuperare i dati da questa response!
         return response.json();
       } else {
-        // vuol dire che la response è arrivata ma che ha un problema
-        // se finiamo nell'else, lanciamoci nel blocco .catch()
         throw new Error('la risposta non era valida');
       }
     })
     .then((data) => {
       console.log('DATI RICEVUTI DAL SERVER', data);
 
-      // prendo un riferimento alla row definita in HTML
       const row = document.getElementById('games-row');
-      // ora devo ciclare l'array "data" e per ogni oggetto (concerto) devo creare
-      // una colonna con dentro una card
 
       data.forEach((game) => {
         row.innerHTML =
@@ -36,23 +26,26 @@ const getGames = function () {
           `
             <div class="col col-12  justify-content-center col-lg-3 col-md-4 col-sm-6 mb-2 mt-1 ">
               <div class="card w-75 border border-secondary shadow-lg">
-              
+     
                 <img src="${game.imageUrl}" class="card-img-top" style="height:200px" alt="${game.name}" />
                 <div class="card-body pb-0  d-flex flex-column bg-secondary" style="height:355px">
                 
                   <h5 class="card-title">${game.name}</h5>
-                  <p class="card-text" style="font-size:0.4em">${game.description}</p>
+                  <p class="card-text" style="font-size:0.4em; text-decoration:none;">${game.description}</p>
                   <div class=" mt-auto mb-1">
-                  <p class="card-text mt-auto" style="font-size:0.4em">${game.brand}</p>
-                  <p class="card-text" style="font-size:0.4em">${game.price}€
+                  <p class="card-text mt-auto" style="font-size:0.4em; text-decoration:none;">${game.brand}</p>
+                  <p class="card-text" style="font-size:0.4em; text-decoration:none;">${game.price}€
                      <div>
-                  <a href="../html/details.html?id=${game._id}" class="btn btn-success style="font-size:0.5em">Giochiamo!</a>
-                  <a href="../html/details.html?id=${game._id}" class="btn btn-success style="font-size:0.em">Modifica</a>
+                  <a href="../html/details.html?id=${game._id}" class="btn btn-success style="font-size:0.5em; text-decoration:none;">Giochiamo!</a>
+                  <a href="../html/details.html?id=${game._id}" class="btn btn-success style="font-size:0.em; text-decoration:none;">Modifica</a>
                 </div>
                 </div>
               </div>
             </div>
           `;
+        // <a href="details.html?id=${game._id}" style=" text-decoration: none"> ho rimosso questo perchè sottolinea tutto e non capisco
+        //bene se i due pulsanti funzionano o meno
+        //cliccando sulla card con questo tag a, l'esercizio funziona
       });
     })
     .catch((error) => {
